7d9a2eeba3e3a1c985d24f6b21c93a25a88c27a7
A-Abbaszadeh <abbas.abbaszadeh@hotmail.com>
2022-05-12 21:18:56 +0430

    Audit Fields With  Shadow Property Created.

--- Persistence/Contexts/DatabaseContext.cs
+++ Persistence/Contexts/DatabaseContext.cs
@@ -1,4 +1,5 @@
 ﻿using Application.Interfaces.Contexts;
+using Domain.Attributes;
 using Domain.Users;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -17,5 +18,55 @@ namespace Persistence.Contexts
 
         }
         public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<User>().Property<DateTime?>("InsertTime");
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                if (entityType.ClrType.GetCustomAttributes(typeof(AuditableAttribute),true).Length > 0)
+                {
+                    modelBuilder.Entity(entityType.Name).Property<DateTime>("InsertTime");
+                    modelBuilder.Entity(entityType.Name).Property<DateTime?>("UpdateTime");
+                    modelBuilder.Entity(entityType.Name).Property<DateTime?>("RemoveTime");
+                    modelBuilder.Entity(entityType.Name).Property<bool>("IsRemoved");
+                }
+            }
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override int SaveChanges()
+        {
+            var modifiedEntities = ChangeTracker.Entries()
+                .Where(p => p.State == EntityState.Added || 
+                p.State == EntityState.Modified || 
+                p.State == EntityState.Deleted);
+
+            foreach (var item in modifiedEntities)
+            {
+                var entityType = item.Context.Model.FindEntityType(item.GetType());
+
+                var inserted = entityType.FindProperty("InsertTime");
+                var updated = entityType.FindProperty("UpdateTime");
+                var removed = entityType.FindProperty("RemoveTime");
+                var isRemoved = entityType.FindProperty("IsRemoved");
+
+                if (item.State == EntityState.Added && inserted is not null)
+                {
+                    item.Property("InsertTime").CurrentValue = DateTime.Now;
+                }
+                else if (item.State == EntityState.Modified && updated is not null)
+                {
+                    item.Property("UpdateTime").CurrentValue = DateTime.Now;
+                }
+                else if (item.State == EntityState.Deleted && removed is not null && isRemoved is not null)
+                {
+                    item.Property("RemoveTime").CurrentValue = DateTime.Now;
+                    item.Property("IsRemoved").CurrentValue = true;
+
+                }
+            }
+            return base.SaveChanges();
+        }
     }
 }
