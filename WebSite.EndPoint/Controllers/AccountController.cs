de9b446a7d0d68fcb906a086a2f1af958ed7c0fd
A-Abbaszadeh <abbas.abbaszadeh@hotmail.com>
2022-05-13 13:27:59 +0430

    User registration created.

--- WebSite.EndPoint/Controllers/AccountController.cs
+++ WebSite.EndPoint/Controllers/AccountController.cs
@@ -1,10 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Domain.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebSite.EndPoint.Models.ViewModel.Register;
 
 namespace WebSite.EndPoint.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<User> _userManager;
+        public AccountController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+
         public IActionResult login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel register)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(register);
+            }
+
+            User newUser = new User
+            {
+                Email = register.Email,
+                UserName = register.Email,
+                FullName = register.FullName,
+                PhoneNumber = register.PhoneNumber
+            };
+
+            var result = _userManager.CreateAsync(newUser, register.Password).Result;
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction(nameof(Profile));
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(error.Code, error.Description);
+            }
+
+            return View(register);
+        }
+
+        public IActionResult Profile()
         {
             return View();
         }
---
+++ WebSite.EndPoint/Models/ViewModel/Register/RegisterViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebSite.EndPoint.Models.ViewModel.Register
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "نام و نام خانوادگی را وارد کنید")]
+        [MaxLength(100, ErrorMessage = "حداکثر طول نام و نام خانوادگی 100 کاراکتر است")]
+        [Display(Name = "نام و نام خانوادگی")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "ایمیل را وارد کنید")]
+        [EmailAddress]
+        [Display(Name = "ایمیل")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "پسورد را وارد کنید")]
+        [DataType(DataType.Password)]
+        [Display(Name = "پسورد")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "تکرار پسورد را وارد کنید")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password),ErrorMessage = "پسورد و تکرار آن باید برابر باشند")]
+        [Display(Name = "تکرار پسورد")]
+        public string Repassword { get; set; }
+
+        [Display(Name = "شماره موبایل")]
+        public string PhoneNumber { get; set; }
+    }
+}
