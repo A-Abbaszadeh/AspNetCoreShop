675214418e7776781be34c26b063833f425f373a
A-Abbaszadeh <abbas.abbaszadeh@hotmail.com>
2022-07-12 23:11:01 +0430

    Create GetListTest

---
+++ UnitTest/Builders/DatabaseBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.Builders
+{
+    public class DatabaseBuilder
+    {
+        internal DatabaseContext GetDbContext()
+        {
+            var option = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            return new DatabaseContext(option);
+        }
+    }
+}
